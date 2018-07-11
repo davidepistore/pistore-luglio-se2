959928fcd9ae231f8a7a8d6a369aa05cb0e7aeda
davidepistore <32167745+davidepistore@users.noreply.github.com>
2018-07-11 11:39:59 +0200

    add feature

--- app.js
+++ app.js
@@ -2,11 +2,57 @@
 const express = require('express'),
     bodyParser = require('body-parser');
 
-const app = express();
+var router = express.Router();
 
 const PORT = process.env.PORT || 3000;
 
 
+var squadre = [];
+var number=0;
+
+function createTeam(id, name, is_still_in, matches){
+	var team = {
+		id: number+1,
+		name: name,
+		is_still_in: is_still_in,
+		matches: matches
+	}
+	number = number + 1;
+	squadre.push(team);
+}
+
+
+router.get('/getTeams', function (req, res) {
+	var id = req.query.id;
+	if(id == undefined){
+		if(!(squadre.length == 0)){
+		res.json(squadre);
+		}else{
+			res.json({
+				"code": 200,
+				"response": "non ci sono squadre"
+			});
+		}
+}});
+
+router.post('/addTeam', function (req, res) {
+	if(req.body.id!=undefined && req.body.name!=undefined && req.body.is_still_in!=undefined && req.body.matches!=undefined){
+    	createTeam(req.body.id, req.body.name, req.body.is_still_in,req.body.matches);
+		res.send("team creato");
+	}else{
+		res.send("unexpected error");
+	}
+})
+
+router.get('/:teamID', function (req, res) {
+    const teamID = req.params.teamID
+    const i = squadre.findIndex(item => {return item.id === teamID})
+    if (i==-1) res.sendStatus(404)
+    else {
+        res.status=200
+        res.json(squadre[i])
+    }
+})
 
 
 app.listen(PORT, function(){
